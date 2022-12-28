b495aea3b4f163f318f982d667689cb169f41757
Sophia <91585668+Sophia9156@users.noreply.github.com>
2022-12-28 11:21:12 +0900

    async storage 추가

--- src/components/layout/Write/WriteHeader.jsx
+++ src/components/layout/Write/WriteHeader.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, View } from "react-native";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
+import { StyleSheet, View, Pressable, Text } from "react-native";
 import TransparentCircleButton from "../../common/TransparentCircleButton";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
-export default function WriteHeader({ onSave, onAskRemove, isEditing }) {
+export default function WriteHeader({ onSave, onAskRemove, isEditing, date, onChangeDate }) {
   const navigation = useNavigation();
   const onGoBack = () => {
     navigation.pop();
+  };
+
+  const [mode, setMode] = useState('date');
+  const [visible, setVisible] = useState(false);
+
+  const onPressDate = () => {
+    setMode('date');
+    setVisible(true);
+  };
+
+  const onPressTime = () => {
+    setMode('time');
+    setVisible(true);
+  };
+
+  const onConfirm = (selectedDate) => {
+    setVisible(false);
+    onChangeDate(selectedDate);
+  };
+
+  const onCancel = () => {
+    setVisible(false);
   };
 
   return (
@@ -30,6 +56,26 @@ export default function WriteHeader({ onSave, onAskRemove, isEditing }) {
           onPress={onSave}
         />
       </View>
+      <View style={styles.center}>
+        <Pressable onPress={onPressDate}>
+          <Text>
+            {format(new Date(date), 'PPP', {
+              locale: ko,
+            })}
+          </Text>
+        </Pressable>
+        <View style={styles.seperator} />
+        <Pressable onPress={onPressTime}>
+          <Text>{format(new Date(date), 'p', { locale: ko })}</Text>
+        </Pressable>
+      </View>
+      <DateTimePickerModal 
+        isVisible={visible}
+        mode={mode}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+        date={date}
+      />
     </View>
   )
 };
@@ -46,4 +92,18 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
   },
+  center: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: -1,
+    flexDirection: 'row'
+  },
+  seperator: {
+    width: 8,
+  }
 });
